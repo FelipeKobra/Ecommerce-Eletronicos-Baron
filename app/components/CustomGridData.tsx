35e8241bb5e6576fa12d93c8594367a6bc6c3401
FelipeKobra <felipekbra@gmail.com>
2024-02-10 20:08:13 -0300

    Update CustomGridData.tsx
    Second try to fix the Data Grid Color on production build, by removing the use of Theme Provider to use directly `sx`

--- app/components/CustomGridData.tsx
+++ app/components/CustomGridData.tsx
@@ -1,46 +1,35 @@
-import { ThemeProvider, createTheme } from "@mui/material/styles";
-import { DataGrid,ptBR } from "@mui/x-data-grid";
+import { DataGrid, ptBR } from "@mui/x-data-grid";
 
 const CustomDataGrid = (props: any) => {
-  const theme = createTheme({
-    components: {
-      MuiTablePagination: {
-        styleOverrides: {
-          root: {
-            color: "oklch(var(--bc)) !important",
-          },
-          selectIcon: {
-            color: "oklch(var(--bc)) !important",
-          },
-          spacer: {
-            color: "oklch(var(--bc)) !important",
-          },
-        },
-      },
-      MuiCheckbox: {
-        styleOverrides: {
-          root: {
-            color: "oklch(var(--bc)) !important",
-          },
-        },
-      },
-    },
-  });
-
   return (
-    <ThemeProvider theme={theme}>
-      <DataGrid
-        experimentalFeatures={{ newEditingApi: true }}
-        className="text-center w-full bg-base-100 text-base-content shadow-xl"
-        localeText={ptBR.components.MuiDataGrid.defaultProps.localeText}
-        pageSize={10}
-        rowsPerPageOptions={[10]}
-        checkboxSelection
-        disableSelectionOnClick
-        autoHeight
-        {...props}
-      />
-    </ThemeProvider>
+    <DataGrid
+      experimentalFeatures={{ newEditingApi: true }}
+      className="text-center w-full bg-base-100 text-base-content shadow-xl"
+      localeText={ptBR.components.MuiDataGrid.defaultProps.localeText}
+      pageSize={10}
+      rowsPerPageOptions={[10]}
+      checkboxSelection
+      disableSelectionOnClick
+      autoHeight
+      sx={{
+        "& .MuiDataGrid-root": {
+          color: "oklch(var(--bc)) !important",
+        },
+        "& .MuiTablePagination-root": {
+          color: "oklch(var(--bc)) !important",
+        },
+        "& .MuiTablePagination-selectIcon": {
+          color: "oklch(var(--bc)) !important",
+        },
+        "& .MuiTablePagination-spacer": {
+          color: "oklch(var(--bc)) !important",
+        },
+        "& .MuiCheckbox-root": {
+          color: "oklch(var(--bc)) !important",
+        },
+      }}
+      {...props}
+    />
   );
 };
 
